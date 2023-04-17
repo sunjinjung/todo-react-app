1764e5b4e2b29d45f805dd4aa3b109126d3c6495
Jung Sun Jin <sunjin9904@gmail.com>
2023-04-17 11:23:50 +0900

    8. React에서 리스트 렌더링하기 (material-ui 반영 전)

--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 class Todo extends React.Component {
     constructor(props){
-        super(props); this.state ={
-            item: props.item }; // 매개변수 item 의 변수/값을 item에 대입
+        super(props);
+        this.state ={ item: props.item }; // 매개변수 item 의 변수/값을 item에 대입
  }
  render(){
     return (
@@ -14,7 +14,6 @@ class Todo extends React.Component {
         />
         <label for={this.state.item.id}>{this.state.item.title}</label>
         </div>
- );
- }
+ ); }
 }
 export default Todo;
